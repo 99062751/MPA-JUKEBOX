0ce23b1f6d03d3022b375bee0690799b6c4aa7d0
99062751 <70890704+99062751@users.noreply.github.com>
2022-06-25 17:10:26 +0200

    recovery commit
    bezig met songs laten zien in de details playlist pagina, ff opslaan zodat ik revert kan doen als het niet goed gaat

--- projects/jukebox/resources/views/playlist/playlist_details.blade.php
+++ projects/jukebox/resources/views/playlist/playlist_details.blade.php
@@ -2,8 +2,8 @@
     <h1>Name playlist:</h1>
     <p>{{ $play["name"] }}</p>
     <h1>Songs playlist:</h1>
-    @foreach($play["songs"] as $s)
-    <p>{{ $s }}</p>
+    @$play["songs"] as $s)
+    <p>{{ $s[0]["duration"] }}</p>
     @endforeach
     <h1>Duration playlist:</h1>
     <p>{{ $play["duration"] }}</p>
--- projects/jukebox/routes/web.php
+++ projects/jukebox/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\GenreController;
 use App\Http\Controllers\SongController;
 use App\Http\Controllers\PlaylistController;
 use App\Models\Playlist;
+use App\Models\Song;
 use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
@@ -40,6 +41,13 @@ Route::get('/playlist/overview/{id}', [PlaylistController::class, "playlist_over
 //Route::get('/playlist/details/{id}', [PlaylistController::class, "playlist_details"])->name("playlist.details");
 Route::get('/playlist/details/{name}', function (Request $request, $name) {
     $playlist= $request->session()->get($name);
+    foreach($playlist[0]["songs"] as $song_id){
+        $song["name"]= Song::where('song_id', '=', $song_id)->get("song_name");
+        $song["artist"]= Song::where('song_id', '=', $song_id)->get("artist");
+        $song["duration"]= Song::where('song_id', '=', $song_id)->get("song_duration");
+        $song["type"]= Song::where('song_id', '=', $song_id)->get("song_type");
+        $playlist[0]["songs"][$song_id] = $song;
+    }
     return view("playlist.playlist_details", ["playlist" => $playlist]);
 })->name("playlist.details");
 
